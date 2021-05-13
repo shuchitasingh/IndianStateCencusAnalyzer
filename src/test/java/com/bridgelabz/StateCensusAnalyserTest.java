160bb522f4202f50b73b4c9ee48692a797502de4
shuchitasingh <singhshuchita5@gmail.com>
2021-05-13 14:13:55 +0530

    Added:file correct but delimeter incorrect

--- src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
@@ -9,6 +9,7 @@ public class StateCensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\Shuchita Singh\\src\\test\\resources\\IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\Shuchita Singh\\src\\main\\resources\\IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_TYPE = "C:\\Users\\Shuchita Singh\\src\\test\\resources\\IndiaStateCensusData.txt";
+    private static final String CSV_FILE_WITH_WRONG_DELIMITER = "C:\\Users\\Shuchita Singh\\src\\main\\resources\\IndiaStateCensusData - Copy.csv";
 
     private Assertions Assert;
 
@@ -53,6 +54,37 @@ public class StateCensusAnalyserTest {
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_TYPE);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+    /**
+     * given indian census file is correct but delimeter wrong should return custom exception....
+     * @throws CensusAnalyserException
+     */
+    @Test
+    public void givenIndianCensusCSVFile_WhenCorrectButDelimiterWrong_ShouldThrowException() throws CensusAnalyserException {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(CSV_FILE_WITH_WRONG_DELIMITER);
+        } catch (CensusAnalyserException x) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, x.type);
+        }
+    }
+
+    /**
+     * when csv file is correct but csv header is incorrect return throw exception
+     * @throws CensusAnalyserException
+     */
+    @Test
+    public void givenIndianCensusCSVFile_WhenCorrectButCSVHeaderIncorrect_ShouldThrowException() throws CensusAnalyserException {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(CSV_FILE_WITH_WRONG_DELIMITER);
+        } catch (CensusAnalyserException x) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, x.type);
         }
     }
 }
